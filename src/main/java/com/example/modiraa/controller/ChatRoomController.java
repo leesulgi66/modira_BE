ea38e58423add7ca21c5cab1397cd96711a4896b
hyeonorof <hyeonorof@gmail.com>
2022-09-14 06:29:40 +0900

    사용 안 하는 코드 정리 및 리팩토링

--- src/main/java/com/example/modiraa/controller/ChatRoomController.java
+++ src/main/java/com/example/modiraa/controller/ChatRoomController.java
@@ -1,9 +1,7 @@
 package com.example.modiraa.controller;
 
 import com.example.modiraa.dto.ChatMessageResponseDto;
-import com.example.modiraa.model.ChatRoom;
 import com.example.modiraa.service.ChatMessageService;
-import com.example.modiraa.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,29 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/chat")
 public class ChatRoomController {
 
-    private final ChatRoomService chatRoomService;
     private final ChatMessageService chatMessageService;
-
-    // 모든 채팅방 목록 반환
-    @GetMapping("/rooms")
-    public List<ChatRoom> roomList() {
-        List<ChatRoom> chatRooms = chatRoomService.findAllRoom();
-        chatRooms.stream().forEach(room -> room.setUserCount(chatRoomService.getUserCount(room.getRoomId())));
-        return chatRooms;
-    }
-
-    // 채팅방 입장 화면
-    @GetMapping("/room/enter/{roomId}")
-    public String roomDetail(@PathVariable String roomId) {
-        return roomId;
-    }
 
     // 해당 채팅방의 메세지 조회
     @GetMapping("/messages/{roomId}")
--- src/main/java/com/example/modiraa/model/ChatMessage.java
+++ src/main/java/com/example/modiraa/model/ChatMessage.java
@@ -1,7 +1,6 @@
 package com.example.modiraa.model;
 
 import com.example.modiraa.dto.ChatMessageRequestDto;
-import com.example.modiraa.service.UserService;
 import lombok.*;
 
 import javax.persistence.*;
@@ -40,16 +39,7 @@ public class ChatMessage {
     private long userCount; // 채팅방 인원수, 채팅방 내에서 메시지가 전달될때 인원수 갱신시 사용
 
     @Builder
-    public ChatMessage(MessageType type, String roomId, Member sender, String message, long userCount) {
-        this.type = type;
-        this.roomId = roomId;
-        this.sender = sender;
-        this.message = message;
-        this.userCount = userCount;
-    }
-
-    @Builder
-    public ChatMessage(ChatMessageRequestDto chatMessageRequestDto, UserService userService) {
+    public ChatMessage(ChatMessageRequestDto chatMessageRequestDto) {
         this.type = chatMessageRequestDto.getType();
         this.roomId = chatMessageRequestDto.getRoomId();
         this.sender =  chatMessageRequestDto.getSender();
