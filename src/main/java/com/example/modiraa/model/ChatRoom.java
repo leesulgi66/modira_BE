fbebe60c9c1e55536a67afe3c68fa3497de2a13d
hyeonorof <hyeonorof@gmail.com>
2022-09-27 22:29:44 +0900

    ChatRoom 사용 안 하는 import 지우기

--- src/main/java/com/example/modiraa/model/ChatRoom.java
+++ src/main/java/com/example/modiraa/model/ChatRoom.java
@@ -3,7 +3,6 @@ package com.example.modiraa.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
